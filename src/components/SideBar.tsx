0cbbffa652adb070e87cf8310b6fd4da73847eb1
hitk1 <luispaulo.degini@gmail.com>
2021-08-30 21:18:33 -0300

    refactor: Refatorações dos modulos

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "../App";
+
+interface GenreProps {
+  handleClick: (id: number) => void
+  genres: GenreResponseProps[]
+  selectedGenreId: number
+}
+
+export function SideBar({ genres, selectedGenreId, handleClick }: GenreProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
